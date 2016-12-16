fdba6dc9c13710d2881a540a0ffba8653b379bdf
Valentin Faugier <valentin.faugier@etu.u-bordeaux.fr>
2016-12-16 18:26:33 +0100

    création de la classe personnage et création d'une instance de celle ci dans preparation.js

--- class/personnage.js
+++ class/personnage.js
@@ -1,3 +1,29 @@
-$.(function() {
-	
-});
+
+	function Personnage(name, sex, level, statCoefs, attacksId) {
+		var self = this;
+
+		// Mise à jour des infos sur le perso
+		self.name = name;
+		self.sex = sex;
+		self.level = level;
+		self.experience = 0;
+
+		//TODO : gérer les attaques
+		self.attacks = [];
+
+		// Mise à jour des stats en fonction des coefficients et du level
+		self.statCoefs = statCoefs;
+		self.lifePoints = self.statCoefs[0]*15 + self.statCoefs[0]*self.level;
+		self.attack = self.statCoefs[1]*10 + self.statCoefs[1]*self.level;
+		self.defense = self.statCoefs[2]*10 + self.statCoefs[2]*self.level;
+		self.speed = self.statCoefs[3]*10 + self.statCoefs[3]*self.level;
+		self.luck = self.statCoefs[4]*10 + self.statCoefs[4]*self.level;
+	}
+
+	Personnage.prototype.levelUp = function() {
+		this.lifePoints += this.statCoefs[0];
+		this.attack += this.statCoefs[1];
+		this.defense += this.statCoefs[2];
+		this.speed += this.statCoefs[3];
+		this.luck += this.statCoefs[4];
+	}
--- preparation/preparation.js
+++ preparation/preparation.js
@@ -1,13 +1,65 @@
 $(function() {
 	$('#validate').on('click', function(event) {
-		var politicien = $('#name').val();
-		if (politicien.length - (politicien.split(" ").length-1) < 3) {
+		var name = $('#name').val();
+		if (name.length - (name.split(" ").length-1) < 3) {
 			event.preventDefault();
 			$('.tropCourt').fadeIn(200);
 			return;
 		}
-			$('.tropCourt').fadeOut(200);
-			localStorage.setItem("name", politicien);
-			localStorage.setItem("man", $('#man').prop("checked"));
+		$('.tropCourt').fadeOut(200);
+		var coefs = distribute(),
+			perso = new Personnage(name, $('#man').prop("checked"), 3, coefs, []);
+			localStorage.setItem("persoPrincipal", perso); //TODO : décomposer l'objet pour pouvoir transiter de pages en pages
 	});
+
+	// Fonction qui retourne un tableau de coefficients de stats en fonction de ce que les gens trouvent le plus important
+	function distribute() {
+		var life = 0,
+			attack = 0,
+			defense = 0,
+			speed = 0,
+			luck = 0,
+			count = 5;
+		while (count > 0) {
+			if (count > 0) {
+				life ++;
+				count --;
+			} else {
+				return ([life, attack, defense, speed, luck]);
+			}
+			if ($('#lie').prop("checked")) {
+				if (count > 0) {
+					attack ++;
+					count --;
+				} else {
+					return ([life, attack, defense, speed, luck]);
+				}
+			}
+			if ($('#doubleTalk').prop("checked")) {
+				if (count > 0) {
+					defense ++;
+					count --;
+				} else {
+					return ([life, attack, defense, speed, luck]);
+				}
+			}
+			if ($('#relations').prop("checked")) {
+				if (count > 0) {
+					speed ++;
+					count --;
+				} else {
+					return ([life, attack, defense, speed, luck]);
+				}
+			}
+			if ($('#rhetoric').prop("checked")) {
+				if (count > 0) {
+					luck ++;
+					count --;
+				} else {
+					return ([life, attack, defense, speed, luck]);
+				}
+			}
+		}
+		return ([life, attack, defense, speed, luck]);
+	}
 });
